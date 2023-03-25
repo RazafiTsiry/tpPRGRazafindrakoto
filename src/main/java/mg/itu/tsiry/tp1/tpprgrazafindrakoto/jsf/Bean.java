68ba09e746c0e87b4dcd8582d1aee03449dcc5f0
Tsiry <razafindrakoto0tsiry@gmail.com>
2023-03-25 16:47:31 +0300

    finition tp2

---
+++ src/main/java/mg/itu/tsiry/tp1/tpprgrazafindrakoto/jsf/Bean.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tsiry.tp1.tpprgrazafindrakoto.jsf;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author tsiry
+ */
+@Named(value = "bean")
+@RequestScoped
+public class Bean {
+
+    private int nombre;
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+
+    public String afficher() {
+        return "affichage_3?nb=" + nombre + "&amp;faces-redirect=true";
+    }
+
+    public String afficher4(){
+        return "affichage_4?nb=" + nombre + "&amp;faces-redirect=true";
+    }
+    
+    public String retour(){
+        return "formulaire_4?nb=" + nombre + "&amp;faces-redirect=true";
+    }
+}
